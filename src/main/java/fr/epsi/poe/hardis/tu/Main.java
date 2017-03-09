79b4539570955c9cee4bdddeb72174aade957496
Jérôme <jeeronimo@laposte.net>
2017-03-09 09:10:19 +0100

    Création Instance Resto affichage avec toString

--- src/main/java/fr/epsi/poe/hardis/tu/Main.java
+++ src/main/java/fr/epsi/poe/hardis/tu/Main.java
@@ -3,6 +3,7 @@ package fr.epsi.poe.hardis.tu;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Restaurant resto = new Restaurant("Le resto", "12, rue du Moulin","01 600","Saint-Bernard","+33123456789");
+	    System.out.println(resto.toString());
     }
 }
